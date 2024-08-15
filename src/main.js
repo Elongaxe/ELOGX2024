2b37f701048fa383720a48d7672a32e9d93cf226
Elongaxe <124607395+Elongaxe@users.noreply.github.com>
2024-08-14 17:13:40 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -36,13 +36,11 @@ directionalLight.position.set(-5, 10, 5);
 directionalLight.castShadow = true;
 scene.add(directionalLight);
 
-const basePath = window.location.pathname.includes('ELOGX2024') ? '/ELOGX2024/' : '/';
-const modelPath = `${basePath}assets/city.glb`;
-
-loader.load(modelPath, function (gltf) {
-    scene.add(gltf.scene);
-});
-
+const loader = new GLTFLoader();
+loader.load('/assets/city.glb', (gltf) => {
+    const city = gltf.scene;
+    city.scale.set(1, 1, 1);
+    scene.add(city);
 
     const composer = new EffectComposer(renderer);
     composer.addPass(new RenderPass(scene, camera));
